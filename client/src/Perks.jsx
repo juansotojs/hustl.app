d622bbff14784c8949683973d3bf8808f4e48f58
Juan Soto <sotoj88515@gmail.com>
2024-07-18 21:44:24 -0700

    first commit

---
+++ client/src/Perks.jsx
@@ -0,0 +1,66 @@
+export default function Perks({selected,onChange}) {
+    function handleCbClick(ev) {
+        const {checked,name} = ev.target;
+        if(checked){
+            onChange([...selected,name]);
+        } else{
+            onChange([...selected.filter(selectedName => selectedName !== name)]);
+        }
+    }
+
+    return (
+        <>
+        <label className="border p-4 flex rounded-2xl gap-2 items-center cursor-pointer">
+                            <input type="checkbox" checked={selected.includes('food')} name="food" onChange={handleCbClick}/>
+                            <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="size-6">
+  <path strokeLinecap="round" strokeLinejoin="round" d="M13.5 21v-7.5a.75.75 0 0 1 .75-.75h3a.75.75 0 0 1 .75.75V21m-4.5 0H2.36m11.14 0H18m0 0h3.64m-1.39 0V9.349M3.75 21V9.349m0 0a3.001 3.001 0 0 0 3.75-.615A2.993 2.993 0 0 0 9.75 9.75c.896 0 1.7-.393 2.25-1.016a2.993 2.993 0 0 0 2.25 1.016c.896 0 1.7-.393 2.25-1.015a3.001 3.001 0 0 0 3.75.614m-16.5 0a3.004 3.004 0 0 1-.621-4.72l1.189-1.19A1.5 1.5 0 0 1 5.378 3h13.243a1.5 1.5 0 0 1 1.06.44l1.19 1.189a3 3 0 0 1-.621 4.72M6.75 18h3.75a.75.75 0 0 0 .75-.75V13.5a.75.75 0 0 0-.75-.75H6.75a.75.75 0 0 0-.75.75v3.75c0 .414.336.75.75.75Z" />
+</svg>
+
+                            <span>Food Vendor</span>
+                        </label>
+                        <label className="border p-4 flex rounded-2xl gap-2 items-center cursor-pointer">
+                            <input type="checkbox" checked={selected.includes('mechanic')} name="mechanic" onChange={handleCbClick}/>
+                            <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="size-6">
+  <path strokeLinecap="round" strokeLinejoin="round" d="M21.75 6.75a4.5 4.5 0 0 1-4.884 4.484c-1.076-.091-2.264.071-2.95.904l-7.152 8.684a2.548 2.548 0 1 1-3.586-3.586l8.684-7.152c.833-.686.995-1.874.904-2.95a4.5 4.5 0 0 1 6.336-4.486l-3.276 3.276a3.004 3.004 0 0 0 2.25 2.25l3.276-3.276c.256.565.398 1.192.398 1.852Z" />
+  <path strokeLinecap="round" strokeLinejoin="round" d="M4.867 19.125h.008v.008h-.008v-.008Z" />
+</svg>
+
+                            <span>Mechanic</span>
+                        </label>
+                        <label className="border p-4 flex rounded-2xl gap-2 items-center cursor-pointer">
+                            <input type="checkbox" checked={selected.includes('barber')} name="barber" onChange={handleCbClick}/>
+                            <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="size-6">
+  <path strokeLinecap="round" strokeLinejoin="round" d="m7.848 8.25 1.536.887M7.848 8.25a3 3 0 1 1-5.196-3 3 3 0 0 1 5.196 3Zm1.536.887a2.165 2.165 0 0 1 1.083 1.839c.005.351.054.695.14 1.024M9.384 9.137l2.077 1.199M7.848 15.75l1.536-.887m-1.536.887a3 3 0 1 1-5.196 3 3 3 0 0 1 5.196-3Zm1.536-.887a2.165 2.165 0 0 0 1.083-1.838c.005-.352.054-.695.14-1.025m-1.223 2.863 2.077-1.199m0-3.328a4.323 4.323 0 0 1 2.068-1.379l5.325-1.628a4.5 4.5 0 0 1 2.48-.044l.803.215-7.794 4.5m-2.882-1.664A4.33 4.33 0 0 0 10.607 12m3.736 0 7.794 4.5-.802.215a4.5 4.5 0 0 1-2.48-.043l-5.326-1.629a4.324 4.324 0 0 1-2.068-1.379M14.343 12l-2.882 1.664" />
+</svg>
+
+                            <span>Barber</span>
+                        </label>
+                        <label className="border p-4 flex rounded-2xl gap-2 items-center cursor-pointer">
+                            <input type="checkbox" checked={selected.includes('trucker')} name="trucker" onChange={handleCbClick}/>
+                            <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="size-6">
+  <path strokeLinecap="round" strokeLinejoin="round" d="M8.25 18.75a1.5 1.5 0 0 1-3 0m3 0a1.5 1.5 0 0 0-3 0m3 0h6m-9 0H3.375a1.125 1.125 0 0 1-1.125-1.125V14.25m17.25 4.5a1.5 1.5 0 0 1-3 0m3 0a1.5 1.5 0 0 0-3 0m3 0h1.125c.621 0 1.129-.504 1.09-1.124a17.902 17.902 0 0 0-3.213-9.193 2.056 2.056 0 0 0-1.58-.86H14.25M16.5 18.75h-2.25m0-11.177v-.958c0-.568-.422-1.048-.987-1.106a48.554 48.554 0 0 0-10.026 0 1.106 1.106 0 0 0-.987 1.106v7.635m12-6.677v6.677m0 4.5v-4.5m0 0h-12" />
+</svg>
+
+                            <span>Trucker</span>
+                        </label>
+                        <label className="border p-4 flex rounded-2xl gap-2 items-center cursor-pointer">
+                            <input type="checkbox" checked={selected.includes('tattooartist')} name="tattooartist" onChange={handleCbClick}/>
+                            <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="size-6">
+  <path strokeLinecap="round" strokeLinejoin="round" d="M9.53 16.122a3 3 0 0 0-5.78 1.128 2.25 2.25 0 0 1-2.4 2.245 4.5 4.5 0 0 0 8.4-2.245c0-.399-.078-.78-.22-1.128Zm0 0a15.998 15.998 0 0 0 3.388-1.62m-5.043-.025a15.994 15.994 0 0 1 1.622-3.395m3.42 3.42a15.995 15.995 0 0 0 4.764-4.648l3.876-5.814a1.151 1.151 0 0 0-1.597-1.597L14.146 6.32a15.996 15.996 0 0 0-4.649 4.763m3.42 3.42a6.776 6.776 0 0 0-3.42-3.42" />
+</svg>
+
+
+                            <span>Tattoo Artist</span>
+
+                        </label>
+                        <label className="border p-4 flex rounded-2xl gap-2 items-center cursor-pointer">
+                            <input type="checkbox" checked={selected.includes('realestate')} name="realestate" onChange={handleCbClick}/>
+                            <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="size-6">
+  <path strokeLinecap="round" strokeLinejoin="round" d="M8.25 21v-4.875c0-.621.504-1.125 1.125-1.125h2.25c.621 0 1.125.504 1.125 1.125V21m0 0h4.5V3.545M12.75 21h7.5V10.75M2.25 21h1.5m18 0h-18M2.25 9l4.5-1.636M18.75 3l-1.5.545m0 6.205 3 1m1.5.5-1.5-.5M6.75 7.364V3h-3v18m3-13.636 10.5-3.819" />
+</svg>
+
+                            <span>Real Estate</span>
+                        </label>
+        </>
+    );
+}
---
+++ client/src/pages/PlacesPage.jsx
@@ -0,0 +1,61 @@
+import { useEffect, useState } from "react";
+import { Link, Navigate, useParams } from "react-router-dom";
+import Perks from "../Perks";
+import axios from "axios";
+import PhotosUploader from "../PhotosUploader";
+import PlacesFormPage from "./PlacesFormPage";
+import AccountNav from "../AccountNav";
+
+
+export default function PlacesPage() {
+    const [places, setPlaces] = useState([]);
+    useEffect(() => {
+        axios.get('/user-posts').then(({data}) => {
+            setPlaces(data);
+        });
+    }, []);
+
+//    const [redirectToPlacesList, setRedirectToPlacesList] = useState(false);
+
+    //if(redirectToPlacesList && action!=='new'){
+      //  return <Navigate to={'/account/places'} />
+    //}
+
+    return (
+        <>
+        <div>
+            <AccountNav/>
+            <h1 className="text-center text-3xl mb-5 -mt-3">My Posts</h1>
+                <div className="text-center">
+                <Link className="inline-flex gap-1 bg-primary text-white py-2 px-6 rounded-full" to={'/account/posts/new'}>
+                <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="size-6">
+            <path stroke-linecap="round" stroke-linejoin="round" d="M12 4.5v15m7.5-7.5h-15" />
+            </svg>
+
+                Add new post
+                </Link>
+            </div>
+            <div className="mt-4">
+                {places.length > 0 && places.map(place => (
+                    <>
+                    <Link to={'/account/posts/'+place._id}className="flex cursor-pointer my-2 gap-4 bg-gray-100 p-4 rounded-2xl">
+                        <div className="flex w-32 h-32 bg-gray-300 shrink-0">
+                            {place.photos.length > 0 && (
+                                <img className="object-cover" src={'http://localhost:4000/uploads/' +place.photos[0]} alt=""/>
+                            )}
+                        </div>
+                        <div className="grow-0 shrink">
+                        <h2 className="text-xl font-bold">
+                        {place.title}
+                        </h2>
+                        <div className="text-sm text-gray-500">{place.address}</div>
+                        <p className="text-sm mt-2">{place.description}</p>
+                        </div>
+                    </Link>
+                    </>
+                ))}
+            </div>
+        </div>
+        </>
+    );
+}
