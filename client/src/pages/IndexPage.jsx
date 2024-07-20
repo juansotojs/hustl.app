ce9ee024dd0da8e0b779f3a4a0784fc3a0c8b9e1
Juan Soto <82733242+juansotojs@users.noreply.github.com>
2024-07-20 16:46:14 -0700

    Update IndexPage.jsx

--- client/src/pages/IndexPage.jsx
+++ client/src/pages/IndexPage.jsx
@@ -19,7 +19,7 @@ import axios from "axios";
           <Link to={'/post/'+place._id}>
             <div className='bg-gray-500 mb-2 rounded-2xl flex'>
             {place.photos?.[0] && (
-              <img className="rounded-2xl object-cover aspect-square" src={'http://localhost:4000/uploads/'+place.photos?.[0]} alt="" />
+              <img className="rounded-2xl object-cover aspect-square" src={'https://hustl-app-api.vercel.app/uploads/'+place.photos?.[0]} alt="" />
             )}
             </div>
             <h3 className='font-bold'>{place.title}</h3>
